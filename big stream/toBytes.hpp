9cc2f07a6c6ce5a69e20eb102e7540a9c2e912bb
czb0737 <czb0737@gmail.com>
2017-07-27 11:47:40 -0700

    Update makefile

--- big stream/toBytes.hpp
+++ big stream/toBytes.hpp
@@ -13,7 +13,6 @@ typedef unsigned char byte;
 template <class T>
 vector<byte> toBytes(const T &t)
 {
-    cout << "Origin: " << t << endl;
     int size = sizeof(t);
     vector<byte> v(size);
     byte *ptr = (byte *)&t;
@@ -23,14 +22,12 @@ vector<byte> toBytes(const T &t)
 
 vector<byte> toBytes(const char *str)
 {
-    cout << "char *: " << str << endl;
     vector<byte> v(str, str + strlen(str));
     return v;
 }
 
 vector<byte> toBytes(const string &s)
 {
-    cout << "string: " << s << endl;
     vector<byte> v(s.begin(), s.end());
     return v;
 }
