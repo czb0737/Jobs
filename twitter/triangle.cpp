45fde0e430d12477fd59c8935d138b6a68087c4c
czb0737 <czb0737@gmail.com>
2017-08-25 15:00:20 -0700

    Triangle

---
+++ twitter/triangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+typedef pair<int, int> Point;
+
+int dist(Point p1, Point p2);
+bool triangle(Point p1, Point p2, Point p3);
+
+int dist(Point p1, Point p2)
+{
+    int dx = p1.first - p2.first;
+    int dy = p1.second - p2.second;
+    return dx * dx + dy * dy;
+}
+
+bool triangle(Point p1, Point p2, Point p3)
+{
+    int dist1 = dist(p1, p2);
+    int dist2 = dist(p1, p3);
+    int dist3 = dist(p2, p3);
+    return (dist1 == dist2) + (dist1 == dist3) + (dist2 == dist3) > 0;
+}
+
+int main(int argc, char **argv)
+{
+    cout << triangle(make_pair(0, 0), make_pair(4, 5), make_pair(5, 7)) << endl;
+}
